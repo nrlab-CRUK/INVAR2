f7793ebd15f9be1a3a843492675373d21b22a0b3
ed5152 <ed515@ic.ac.uk>
2022-05-03 14:43:09 +0100

    Filtering sizeTable df for correct probability distribution

--- R/4_detection/generalisedLikelihoodRatioTest.R
+++ R/4_detection/generalisedLikelihoodRatioTest.R
@@ -159,6 +159,9 @@ calculateLikelihoodRatioForSampleWithSize <- function(mutationsTable, sizeTable,
 
     mutationsTable <- mutationsTable %>%
         mutate(DP = 1)
+    
+    # Filter dataframe to include only minFragmentLength:maxFragmentLength fragments - affects the weight normalisation
+    sizeTable <- filter(sizeTable, SIZE>=minFragmentLength, SIZE<=maxFragmentLength)
 
     ## read length probabilites for mutant reads
 
