d6db8b9b16270f6d3b3348f93d7b96a0ec70a14e
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2022-07-19 16:12:56 +0100

    Revert "Moved to samtools+bcftools 1.15, which don't have the clash in conda."
    This reverts commit 59f8e65c46ffa47e003736b519d9c495fda39ced.

--- container/makesingularity.sh
+++ container/makesingularity.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-# Builds a Singularity image from the Docker image.
-# Use makedocker.sh first.
+if [[ -d spython ]]
+then
+    source spython/bin/activate
+else
+    python3 -m venv spython
+    source spython/bin/activate
+    pip install spython
+fi
+
+spython recipe Dockerfile > singularity_spec.txt
+
+sudo rm -rf invar_sandbox
+sudo singularity build --sandbox invar_sandbox singularity_spec.txt
+
+
 # See https://stackoverflow.com/a/60316979
-
-TAG="latest"
-REPO="nrlabcruk/invar2:$TAG"
-
-sudo rm -rf invar_sandbox invar.sif
-
-#sudo singularity build --sandbox invar_sandbox docker-daemon://${REPO}
-sudo singularity build invar.sif docker-daemon://${REPO}
-
+# sudo singularity build my_container.sif docker-daemon://local/my_container
