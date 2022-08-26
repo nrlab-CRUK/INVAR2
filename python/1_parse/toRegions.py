d9d9fa2eead4028f64e86dc89d9fb418b2913c04
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2022-08-26 13:24:49 +0100

    Removed trimming and readding of chromosome prefix string.

--- python/1_parse/toRegions.py
+++ python/1_parse/toRegions.py
@@ -3,19 +3,15 @@
 # Build a file of regions for Tabix and samtools.
 
 import csv
-import re
 import sys
 
 # sys.argv[0] is the script path.
 mutationsFile = sys.argv[1]
 offset = int(sys.argv[2]) if len(sys.argv) > 2 else 0
-prefix = sys.argv[3] if len(sys.argv) > 3 else ""
-
-chrRemover = re.compile(r'^chr', re.IGNORECASE)
 
 with open(mutationsFile, 'r') as fp1:
     csvReader = csv.DictReader(fp1, delimiter = '\t')
     for mutation in csvReader:
-        chr = chrRemover.sub("", mutation['CHROM']).upper()
+        chr = mutation['CHROM']
         pos = int(mutation['POS'])
-        print(f"{prefix}{chr}:{pos - offset}-{pos + offset}")
+        print(f"{chr}:{pos - offset}-{pos + offset}")
