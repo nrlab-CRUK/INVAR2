d3e40073a47257bc2b09a1534b93bbb34d3b1279
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2025-02-20 10:37:07 +0000

    Separate pushing docker image from building.

--- container/makedocker.sh
+++ container/makedocker.sh
@@ -4,4 +4,4 @@ TAG="1.0.3"
 REPO="nrlabcruk/invar2:$TAG"
 
 sudo docker build --tag "$REPO" --file Dockerfile .
-sudo docker push "$REPO"
+# sudo docker push "$REPO"
---
+++ container/pushdocker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+TAG="1.0.3"
+REPO="nrlabcruk/invar2:$TAG"
+
+sudo docker push "$REPO"
